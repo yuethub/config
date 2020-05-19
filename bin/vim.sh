37fac3bcfdd2e7445a3d326936c104e7f6bc2022
Yue Tang <yuet_mail@163.com>
2020-05-19 17:02:41 +0800

    modified some files

--- bin/vim.sh
+++ bin/vim.sh
@@ -5,8 +5,10 @@ cTemplate()
     if [ ! -f $1 ] && [ ! -d $1 ]; then
         printf "#include <stdio.h>\n" >> $1
         printf "\n" >> $1
-        printf "int main(int argc, char *argv[])\n" >> $1
-        printf "{\n\n}\n" >> $1
+        if [ "$1" = "main.c" ]; then
+            printf "int main(int argc, char *argv[])\n" >> $1
+            printf "{\n\n}\n" >> $1
+        fi
     fi
 }
 
@@ -25,31 +27,37 @@ hTemplate()
 cppTemplate()
 {
     if [ ! -f $1 ] && [ ! -d $1 ]; then
-        preffix=$(echo $1 | cut -d '.' -f 1)
+        if [ "$1" = "main.cpp" ]; then
+            printf "#include <iostream>\n\n" >> $1
+            printf "int main(int argc, char *argv[])\n" >> $1
+            printf "{\n\n}\n" >> $1
+        else
+            preffix=$(echo $1 | cut -d '.' -f 1)
 
-        # create c++ header file
-        hfile="${preffix}.h"
-        if [ ! -f $hfile ];then
-            printf "#ifndef _${preffix^^}_H_\n" >> $hfile
-            printf "#define _${preffix^^}_H_\n" >> $hfile
-            printf "\n" >> $hfile
-            printf "class ${preffix^}\n" >> $hfile
-            printf "{\npublic:\n" >> $hfile
-            printf "\t${preffix^}();\n" >> $hfile
-            printf "\t~${preffix^}();\n\n" >> $hfile
-            printf "private:\n\n" >> $hfile
-            printf "};\n\n\n" >> $hfile
-            printf "#endif\n" >> $hfile
+            # create c++ header file
+            hfile="${preffix}.h"
+            if [ ! -f $hfile ];then
+                printf "#ifndef _${preffix^^}_H_\n" >> $hfile
+                printf "#define _${preffix^^}_H_\n" >> $hfile
+                printf "\n" >> $hfile
+                printf "class ${preffix^}\n" >> $hfile
+                printf "{\npublic:\n" >> $hfile
+                printf "\t${preffix^}();\n" >> $hfile
+                printf "\t~${preffix^}();\n\n" >> $hfile
+                printf "private:\n\n" >> $hfile
+                printf "};\n\n\n" >> $hfile
+                printf "#endif\n" >> $hfile
+            fi
+
+            # create C++ source file
+            printf "#include \"$hfile\"\n\n"  >> $1
+            printf "${preffix^}::${preffix^}()\n" >> $1 
+            printf "{\n\n}\n\n" >> $1
+            printf "${preffix^}::~${preffix^}()\n" >> $1
+            printf "{\n\n}\n\n" >> $1
+            unset hfile
+            unset preffix
         fi
-
-        # create C++ source file
-        printf "#include \"$hfile\"\n\n"  >> $1
-        printf "${preffix^}::${preffix^}()\n" >> $1 
-        printf "{\n\n}\n\n" >> $1
-        printf "${preffix^}::~${preffix^}()\n" >> $1
-        printf "{\n\n}\n\n" >> $1
-        unset hfile
-        unset preffix
     fi
 }
 
